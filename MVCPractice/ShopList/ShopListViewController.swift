6aef5c8ff32bec41972d0c1435b2f664369db726
haruto-matsushima <matsu_haruto@icloud.com>
2021-08-25 21:17:18 +0900

    add shoplist-file

---
+++ MVCPractice/ShopList/ShopListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShopListViewController.swift
+//  MVCPractice
+//
+//  Created by 松島悠人 on 2021/08/25.
+//
+
+import UIKit
+
+class ShopListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
